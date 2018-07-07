986edb28536d44b8a5c2a318fcb30d34a8b53a8d
Camila Oliveira <bruno.poc@gmail.com>
2018-07-07 16:04:14 -0300

    fazendo alguns ajustes basicos

---
+++ application/views/template/frontend/header.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Blog for Study</title>
+	<link rel='stylesheet' type="text/css" href="<?= asset_url('css/style.css') ?>">
+	
+</head>
+
+<body>
+	Há!
+</body>
+</html>
